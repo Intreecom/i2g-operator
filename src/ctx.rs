9b2d7110fcfe0b0ab9b2e3fc1faaf6b28629fa65
Pavel Kirilin <win10@list.ru>
2025-12-09 18:27:42 +0100

    Fixed error when hostname was required.

--- src/ctx.rs
+++ src/ctx.rs
@@ -1,6 +1,7 @@
 use std::sync::{Arc, atomic::AtomicBool};
 
 use clap::Parser;
+use rand::distr::{Alphanumeric, SampleString};
 
 use crate::args::I2GArgs;
 
@@ -17,7 +18,11 @@ impl Context {
         let args = I2GArgs::parse();
         let client = kube::Client::try_default().await?;
         let is_leader = Arc::new(AtomicBool::new(false));
-        let hostname = std::env::var("HOSTNAME").or_else(|_| std::env::var("HOST"))?;
+        let mut rng = rand::rng();
+        let prefix = Alphanumeric.sample_string(&mut rng, 12);
+        let hostname = std::env::var("HOSTNAME")
+            .or_else(|_| std::env::var("HOST"))
+            .unwrap_or_else(|_| format!("i2g-operator-{prefix}"));
         Ok(Context {
             args,
             client,
